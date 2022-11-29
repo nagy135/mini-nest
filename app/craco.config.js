f6221a45fca8540fd49d7dfe9d281714a0a46fce
Viktor Nagy <viktor.nagy@01people.com>
2022-11-29 11:22:06 +0100

    feat(app): craco, tailwind, daisyui

---
+++ app/craco.config.js
@@ -0,0 +1,15 @@
+/* craco.config.js */
+const path = require(`path`);
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@ctypes": path.resolve(__dirname, "src/ctypes"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@api": path.resolve(__dirname, "src/api"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+};
