3a080271e2edff70f30a7b02c3b7adac03df56a6
Viktor Nagy <viktor.nagy@01people.com>
2022-11-30 12:12:55 +0100

    feat(api): cors prod/local setup

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://mini-app.infiniter.eu'
+    ],
+    methods: ['POST', 'PUT', 'DELETE', 'GET', 'OPTIONS', 'HEAD'],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
