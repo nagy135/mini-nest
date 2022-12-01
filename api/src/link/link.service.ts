7cb2909641fd057727094eaddc889504cef752c6
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-12-01 09:10:53 +0100

    feat(api): resolve duplicate link names + refactor randomString util

--- api/src/link/link.service.ts
+++ api/src/link/link.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { randomString } from '@utils/common';
 import { Repository } from 'typeorm';
 import { Link } from './link.entity';
 
@@ -12,15 +13,23 @@ export class LinkService {
   }
 
   async createNew(url: string, name?: string): Promise<Link> {
+    if (name && (await this.findOneByName(name))) {
+      throw new ConflictException(
+        'Choose different name...conflict with another link',
+      );
+    }
     const link = this.linkRepository.create({
       url,
-      name: name ? name : (Math.random() + 1).toString(36).substring(7), // TODO: this can have clashes!
+      name: name ? name : randomString(7),
     });
+    while (await this.findOneByName(link.name)) {
+      link.name = randomString(7);
+    }
     await link.save();
     return link;
   }
 
-  async findOneByName(name: string): Promise<Link> {
+  async findOneByName(name: string): Promise<Link | null> {
     return this.linkRepository.findOne({ where: { name } });
   }
 }
